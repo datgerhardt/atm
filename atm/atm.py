ddc5f5387a6682da9fdaf9834dfd374ec4a00410
Datsomor Gerhardt Kwame <datsomorg@gmail.com>
2022-11-02 20:35:01 +0000

    Init

---
+++ atm/atm.py
@@ -0,0 +1,43 @@
+from typing import Dict, List
+
+
+class ATM:
+
+    def __init__(self, list_denominations: List[int] = [1, 2, 5], amt: int = 0) -> None:
+        self.list_denominations = list_denominations
+        # The sort the list in the desc. order
+        self.list_denominations.sort(reverse=True)
+        self.amt = amt
+        # key is string denomination and value is the number of each denomination notes
+        self.denominations = {}
+
+    def depense(self) -> Dict[str, int]:
+        amt = self.amt
+        for i in self.list_denominations:
+            # print(i, self.list_denominations)
+            assert i != 0, ZeroDivisionError  # "Zero Division Error"
+            num = amt // i
+            rem = amt - num * i
+
+            if rem == 0:
+                self.denominations[str(i)] = num
+                break
+            else:
+                if num != 0:
+                    self.denominations[str(i)] = num
+                amt = rem
+                continue
+        return self.denominations
+
+    def get_amt_by_denominations(self, amtDict: Dict[str, int]) -> Dict[str, int]:
+        for i in amtDict.keys():
+            self.amt += amtDict[i] * int(i)
+        return self.depense()
+
+
+denominations = {}
+list_denominations = [1, 2, 5]
+amt = 111
+
+atm = ATM(list_denominations=list_denominations, amt=amt)
+print("Class Base: ", atm.depense())
---
+++ test.py
@@ -0,0 +1,28 @@
+
+import unittest
+
+from atm.atm import ATM
+
+
+class TestATM(unittest.TestCase):
+    def test_class_depense(self):
+        atm = ATM([1, 2, 5], 111)
+        self.assertEqual(atm.depense(), {
+                         '1': 1, '5': 22},  "Should be {'1':1,'5':22}")
+
+    def test_get_amt_by_denominations(self):
+        atm = ATM([1, 2, 5])
+        self.assertEqual(atm.get_amt_by_denominations({'1': 1, '5': 22, '50': 10}), {
+                         '5': 122, '1': 1},  "Should be {'5': 122, '1': 1}")
+
+    def test_bad_type(self):
+        with self.assertRaises(TypeError):
+            atm = ATM([1, '2', 5])
+
+    def test_denominations_with_zero(self):
+        with self.assertRaises(ZeroDivisionError):
+            atm = ATM([0])
+
+
+if __name__ == "__main__":
+    unittest.main()
